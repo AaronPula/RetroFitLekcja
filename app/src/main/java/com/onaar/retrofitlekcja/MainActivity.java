5b179833b80c45720245bc61ee23c4c8742610c4
AaronPula <aaron.pula1@gmail.com>
2026-09-22 12:14:57 +0200

    link do json

--- app/src/main/java/com/onaar/retrofitlekcja/MainActivity.java
+++ app/src/main/java/com/onaar/retrofitlekcja/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+//    https://my-json-server.typicode.com/AaronPula/RetroFitLekcja/pytania
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
